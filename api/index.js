a5f961f36a86cda6ca76f33f63177902c5417082
muboxgithub <mubagithub@gmail.com>
2025-10-05 23:50:16 +0300

    my commitdhhh

--- api/index.js
+++ api/index.js
@@ -8,30 +8,30 @@ app.use(
   })
 );
 
-const { PrismaClient } = require("./generated/prisma");
+const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const passport = require("./middlewares/passportConfig"); // Load strategies
+const passport = require("../middlewares/passportConfig"); // Load strategies
 app.use(passport.initialize());
 
 //user
-const userRoute = require("./routes/userRoute");
+const userRoute = require("../routes/userRoute");
 app.use("/api/users", userRoute);
 
 //auth
-const authRoute = require("./routes/authRoute");
+const authRoute = require("../routes/authRoute");
 app.use("/api/auth", authRoute);
 
 //categories
-const categorieRoute = require("./routes/categorieRoute");
+const categorieRoute = require("../routes/categorieRoute");
 app.use("/api/categories", categorieRoute);
 
 //products
-const productRoute = require("./routes/productRoute");
+const productRoute = require("../routes/productRoute");
 app.use("/api/products", productRoute);
 
 //====get categories
@@ -47,7 +47,7 @@ app.get("/test-categ", async (req, res) => {
   }
 });
 //product images
-const productImageRoute = require("./routes/productImageRoute");
+const productImageRoute = require("../routes/productImageRoute");
 app.use("/api/products", productImageRoute);
 
 app.listen(process.env.PORT || 8000, () => {
